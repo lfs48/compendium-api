059d1ee78bbf9ba4f21b464768f4c0611a7150e4
Lucas <lucasfschraier@gmail.com>
2022-08-22 17:57:10 -0400

    feat: create races table in db

---
+++ db/migrate/20220822151407_create_races.rb
@@ -0,0 +1,12 @@
+class CreateRaces < ActiveRecord::Migration[7.0]
+  def change
+    create_table :races, id: :uuid do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.string :type, null: false
+      t.string :size, null: false
+      
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_203151) do
+ActiveRecord::Schema.define(version: 2022_08_22_151407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2022_02_06_203151) do
     t.string "description", null: false
     t.string "kind", null: false
     t.integer "level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "races", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.string "type", null: false
+    t.string "size", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
