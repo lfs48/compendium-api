d5fceaf5223a30357e61eedc70b9cc3ce485cca3
Lucas <lucasfschraier@gmail.com>
2022-09-07 11:01:48 -0400

    feat: add prereq col to boons

--- db/migrate/20220906014723_create_boons.rb
+++ db/migrate/20220906014723_create_boons.rb
@@ -3,6 +3,7 @@ class CreateBoons < ActiveRecord::Migration[7.0]
     create_table :boons, id: :uuid do |t|
       t.string :name, null: false
       t.string :description, null: false
+      t.string :prereq
       t.uuid :source_id, foreign_key: true
       t.string :source_type
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_09_06_014723) do
   create_table "boons", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "description", null: false
+    t.string "prereq"
     t.uuid "source_id"
     t.string "source_type"
     t.datetime "created_at", precision: 6, null: false
