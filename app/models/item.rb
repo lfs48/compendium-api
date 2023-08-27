de25bab73c2a44dd877a63a1b62e56eeea972eed
Lucas <lucasfschraier@gmail.com>
2023-08-27 13:08:42 -0400

    feat: implement items model

---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+
+    validates :name, 
+    :description, 
+    :kind,
+    :rarity,
+    :bulk,
+    :value,
+    presence: true
+
+    validates :kind, inclusion: { in: %w(armor weapon tool commodity gear treasure wondrous consumable), message: "invalid kind"}
+    validates :rarity, inclusion: { in: %w(common uncommon rare legendary artifact), message: "invalid rarity"}
+    validates :value, format: { with: /\A\d+\z|\d+d\d+(\z|x\d+\z)/, message: "invalid value"}
+
+end
