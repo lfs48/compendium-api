fd4bbe4bf0524f610c70bc413fa918f936600403
Lucas <lucasfschraier@gmail.com>
2023-08-27 18:41:53 -0400

    fix: replace 'x' in item value string with '*'

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,6 @@ class Item < ApplicationRecord
 
     validates :kind, inclusion: { in: %w(armor weapon tool commodity gear treasure wondrous consumable), message: "invalid kind"}
     validates :rarity, inclusion: { in: %w(common uncommon rare legendary artifact), message: "invalid rarity"}
-    validates :value, format: { with: /\A\d+\z|\d+d\d+(\z|x\d+\z)/, message: "invalid value"}
+    validates :value, format: { with: /\A\d+\z|\d+d\d+(\z|\*\d+\z)/, message: "invalid value"}
 
 end
