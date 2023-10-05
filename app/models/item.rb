6c70be19b92d1a111805eab98c670998ed27729b
Lucas <lucasfschraier@gmail.com>
2023-10-05 12:46:51 -0400

    feat: remove item model bulk regex validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,5 @@ class Item < ApplicationRecord
 
     validates :kind, inclusion: { in: %w(armor weapon tool commodity gear treasure wondrous consumable), message: "invalid kind"}
     validates :rarity, inclusion: { in: %w(common uncommon rare legendary artifact), message: "invalid rarity"}
-    validates :value, format: { with: /\A\d+\z|\d+d\d+(\z|\*\d+\z)/, message: "invalid value"}
 
 end
