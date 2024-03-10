f53abbcc5e7fffd3484c5cd1781f178df526d878
Lucas <lucasfschraier@gmail.com>
2024-03-10 10:17:00 -0400

    feat: broadcast message creation to actioncable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
 
+    after_create_commit { broadcast_message }
+
     validates :body, 
     :kind,
     :user_id,
@@ -8,5 +10,16 @@ class Message < ApplicationRecord
     validates :kind, inclusion: { in: %w(text roll), message: "invalid kind"}
 
     belongs_to :user
+
+    private
+
+    def broadcast_message
+        ActionCable.server.broadcast('MessagesChannel', {
+            id:,
+            body:,
+            created_at:,
+            user: user.username
+        })
+    end
 
 end
