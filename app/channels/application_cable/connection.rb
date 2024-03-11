407f2a69e51b35bc5826be5cb58f467e048773ae
Lucas <lucasfschraier@gmail.com>
2024-03-11 09:56:00 -0400

    feat: implement jwt authentication when connecting to actioncable server

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,51 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if verified_user = current_user_id
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
+    def jwt_secret
+      ENV["JWT_SECRET"]
+    end
+
+    private
+
+    def decode_jwt
+      begin
+        token = request.headers["Authorization"].split(' ')[1]
+        decoded_array = JWT.decode( token, jwt_secret, true, { algorithm: 'HS256' } )
+        payload = decoded_array.first
+        return payload
+      rescue #JWT::VerificationError
+        return nil
+      end
+    end
+
+    def current_user_id
+      decoded = decode_jwt
+      return decoded['sub']
+    end
+
+    def client_has_valid_token?
+      begin
+        user = User.find(current_user_id)
+        return !!user
+      rescue
+        return false
+      end
+    end
+
   end
 end
